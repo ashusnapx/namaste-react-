f2d7433f79a7d486dcd8bda45fe4d9f0a0485acd
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-07-09 15:35:34 +0530

    heavy

---
+++ DAY - 2/dist/index.6bd02f5a.js
@@ -0,0 +1,21 @@
+const heading = React.createElement("h1", {
+    id: "title"
+}, "this is heading with size h1");
+/* 
+same as <h1 id="root">this is heading with size h1</h1>
+*/ const heading2 = React.createElement("h2", {
+    id: "container"
+}, "this is heading with size h2");
+const conatiner = React.createElement("div", {
+    id: "container"
+}, [
+    heading,
+    heading2
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render([
+    heading,
+    heading2
+]);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
